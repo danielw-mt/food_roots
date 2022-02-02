cb189224966e2761581bd23d1189860395d63d98
Daniel <Daniel.wag@gmx.net>
2022-02-02 10:47:10 +0000

    added all essential files

---
+++ Food-Roots/lib/main.dart
@@ -0,0 +1,158 @@
+import 'package:flutter/material.dart';
+import 'package:flutterapp/food_20rootsapp/generatedmenuwidget/GeneratedMenuWidget.dart';
+import 'package:flutterapp/food_20rootsapp/generatedhomescreenwidget/GeneratedHomeScreenWidget.dart';
+import 'package:flutterapp/food_20rootsapp/generatedrecipeswidget1/GeneratedRecipesWidget1.dart';
+import 'package:flutterapp/food_20rootsapp/generatedframe2608133widget/GeneratedFrame2608133Widget.dart';
+import 'package:flutterapp/food_20rootsapp/generatedgroup17widget2/GeneratedGroup17Widget2.dart';
+import 'package:flutterapp/food_20rootsapp/generatedrecipedetailswidget/GeneratedRecipeDetailsWidget.dart';
+import 'package:flutterapp/food_20rootsapp/generatednewsdetailswidget/GeneratedNewsDetailsWidget.dart';
+import 'package:flutterapp/food_20rootsapp/generatednewsdetailsscrollwidget/GeneratedNewsdetailsscrollWidget.dart';
+import 'package:flutterapp/food_20rootsapp/generatedarticledetailswidget/GeneratedArticleDetailsWidget.dart';
+import 'package:flutterapp/food_20rootsapp/generatedfavoritesproductswidget/GeneratedFavoritesProductsWidget.dart';
+import 'package:flutterapp/food_20rootsapp/generatedfavoritesproductswidget1/GeneratedFavoritesProductsWidget1.dart';
+import 'package:flutterapp/food_20rootsapp/generatedfavoritesrecipeswidget/GeneratedFavoritesRecipesWidget.dart';
+import 'package:flutterapp/food_20rootsapp/generatedsearchwidget8/GeneratedSearchWidget8.dart';
+import 'package:flutterapp/food_20rootsapp/generatedsearchinputwidget/GeneratedSearchInputWidget.dart';
+import 'package:flutterapp/food_20rootsapp/generatedfirstlevelwidget/GeneratedFirstLevelWidget.dart';
+import 'package:flutterapp/food_20rootsapp/generatedfirstlevelwidget1/GeneratedFirstLevelWidget1.dart';
+import 'package:flutterapp/food_20rootsapp/generatedfirstlevelwidget2/GeneratedFirstLevelWidget2.dart';
+import 'package:flutterapp/food_20rootsapp/generatedsearchinputwidget1/GeneratedSearchInputWidget1.dart';
+import 'package:flutterapp/food_20rootsapp/generatedgroup9widget/GeneratedGroup9Widget.dart';
+import 'package:flutterapp/food_20rootsapp/generatedsearchtypingwidget/GeneratedSearchTypingWidget.dart';
+import 'package:flutterapp/food_20rootsapp/generatedlisteditionwidget/GeneratedListEditionWidget.dart';
+import 'package:flutterapp/food_20rootsapp/generatedsettingsemptywidget/GeneratedSettingsEmptyWidget.dart';
+import 'package:flutterapp/food_20rootsapp/generatedhomewidget25/GeneratedHomeWidget25.dart';
+import 'package:flutterapp/food_20rootsapp/generatedhomearticleswidget/GeneratedHomeArticlesWidget.dart';
+import 'package:flutterapp/food_20rootsapp/generatedhomenewswidget/GeneratedHomeNewsWidget.dart';
+import 'package:flutterapp/food_20rootsapp/generatedhomevideoswidget/GeneratedHomeVideosWidget.dart';
+import 'package:flutterapp/food_20rootsapp/generateduserwidget/GeneratedUserWidget.dart';
+import 'package:flutterapp/food_20rootsapp/generatedproductsupplychainwidget/GeneratedProductSupplyChainWidget.dart';
+import 'package:flutterapp/food_20rootsapp/generatedsupplychainwidget/GeneratedSupplychainWidget.dart';
+import 'package:flutterapp/food_20rootsapp/generatedproducthealthwidget/GeneratedProductHealthWidget.dart';
+import 'package:flutterapp/food_20rootsapp/generatedproductstoreswidget/GeneratedProductStoresWidget.dart';
+import 'package:flutterapp/food_20rootsapp/generatedstorewidget/GeneratedStoreWidget.dart';
+import 'package:flutterapp/food_20rootsapp/generatedgroup10widget/GeneratedGroup10Widget.dart';
+import 'package:flutterapp/food_20rootsapp/generatedarticlesscrollwidget/GeneratedArticlesScrollWidget.dart';
+import 'package:flutterapp/food_20rootsapp/generatednewsscrollwidget/GeneratedNewsscrollWidget.dart';
+import 'package:flutterapp/food_20rootsapp/generatednewsdetailwidget/GeneratedNewsdetailWidget.dart';
+import 'package:flutterapp/food_20rootsapp/generatedarticledetailwidget1/GeneratedArticleDetailWidget1.dart';
+import 'package:flutterapp/food_20rootsapp/generatedarticledetailwidget2/GeneratedArticledetailWidget2.dart';
+import 'package:flutterapp/food_20rootsapp/generatedarticleswidget1/GeneratedArticlesWidget1.dart';
+import 'package:flutterapp/food_20rootsapp/generatedvideoswidget1/GeneratedVideosWidget1.dart';
+import 'package:flutterapp/food_20rootsapp/generatednewswidget1/GeneratedNewsWidget1.dart';
+// import 'package:flutterapp/food_20rootsapp/generatednewsdetailswidget/GeneratedNewsDetailsWidget.dart';
+// import 'package:flutterapp/food_20rootsapp/generatedarticledetailswidget/GeneratedArticleDetailsWidget.dart';
+import 'package:flutterapp/food_20rootsapp/generatedhomescreenarticledetailswidget/GeneratedHomeScreenarticledetailsWidget.dart';
+import 'package:flutterapp/food_20rootsapp/generatedvideosscrollwidget2/GeneratedVideosscrollWidget2.dart';
+import 'package:flutterapp/food_20rootsapp/generatedbasicdialogwidget/GeneratedBasicdialogWidget.dart';
+import 'package:flutterapp/food_20rootsapp/generatedbasicdialogwidget1/GeneratedBasicdialogWidget1.dart';
+import 'package:flutterapp/food_20rootsapp/generatednewsscrollwidget1/GeneratedNewsscrollWidget1.dart';
+import 'package:flutterapp/food_20rootsapp/generatedarticlesscrollwidget1/GeneratedArticlesscrollWidget1.dart';
+import 'package:flutterapp/food_20rootsapp/generatedvideosscrollwidget3/GeneratedVideosscrollWidget3.dart';
+import 'package:flutterapp/food_20rootsapp/generatedsearchscrollwidget1/GeneratedSearchscrollWidget1.dart';
+import 'package:flutterapp/food_20rootsapp/generatedrecipedetailsscrollwidget/GeneratedRecipedetailsscrollWidget.dart';
+
+void main() {
+  runApp(Food_20RootsApp());
+}
+
+class Food_20RootsApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      initialRoute: '/GeneratedHomeWidget25',
+      routes: {
+        '/GeneratedMenuWidget': (context) => GeneratedMenuWidget(),
+        '/GeneratedHomeScreenWidget': (context) => GeneratedHomeScreenWidget(),
+        '/GeneratedRecipesWidget1': (context) => GeneratedRecipesWidget1(),
+        '/GeneratedFrame2608133Widget': (context) =>
+            GeneratedFrame2608133Widget(),
+        '/GeneratedGroup17Widget2': (context) => GeneratedGroup17Widget2(),
+        '/GeneratedRecipeDetailsWidget': (context) =>
+            GeneratedRecipeDetailsWidget(),
+        '/GeneratedNewsDetailsWidget': (context) =>
+            GeneratedNewsDetailsWidget(),
+        '/GeneratedNewsdetailsscrollWidget': (context) =>
+            GeneratedNewsdetailsscrollWidget(),
+        // '/GeneratedArticleDetailsWidget': (context) =>
+        //     GeneratedArticleDetailsWidget(),
+        '/GeneratedFavoritesProductsWidget': (context) =>
+            GeneratedFavoritesProductsWidget(),
+        '/GeneratedFavoritesProductsWidget1': (context) =>
+            GeneratedFavoritesProductsWidget1(),
+        '/GeneratedFavoritesRecipesWidget': (context) =>
+            GeneratedFavoritesRecipesWidget(),
+        '/GeneratedSearchWidget8': (context) => GeneratedSearchWidget8(),
+        '/GeneratedSearchInputWidget': (context) =>
+            GeneratedSearchInputWidget(),
+        '/GeneratedFirstLevelWidget': (context) => GeneratedFirstLevelWidget(),
+        '/GeneratedFirstLevelWidget1': (context) =>
+            GeneratedFirstLevelWidget1(),
+        '/GeneratedFirstLevelWidget2': (context) =>
+            GeneratedFirstLevelWidget2(),
+        '/GeneratedSearchInputWidget1': (context) =>
+            GeneratedSearchInputWidget1(),
+        '/GeneratedGroup9Widget': (context) => GeneratedGroup9Widget(),
+        '/GeneratedSearchTypingWidget': (context) =>
+            GeneratedSearchTypingWidget(),
+        '/GeneratedListEditionWidget': (context) =>
+            GeneratedListEditionWidget(),
+        '/GeneratedSettingsEmptyWidget': (context) =>
+            GeneratedSettingsEmptyWidget(),
+        '/GeneratedHomeWidget25': (context) => GeneratedHomeWidget25(),
+        '/GeneratedHomeArticlesWidget': (context) =>
+            GeneratedHomeArticlesWidget(),
+        '/GeneratedHomeNewsWidget': (context) => GeneratedHomeNewsWidget(),
+        '/GeneratedHomeVideosWidget': (context) => GeneratedHomeVideosWidget(),
+        '/GeneratedUserWidget': (context) => GeneratedUserWidget(),
+        '/GeneratedProductSupplyChainWidget': (context) =>
+            GeneratedProductSupplyChainWidget(),
+        '/GeneratedSupplychainWidget': (context) =>
+            GeneratedSupplychainWidget(),
+        '/GeneratedProductHealthWidget': (context) =>
+            GeneratedProductHealthWidget(),
+        '/GeneratedProductStoresWidget': (context) =>
+            GeneratedProductStoresWidget(),
+        '/GeneratedStoreWidget': (context) => GeneratedStoreWidget(),
+        '/GeneratedGroup10Widget': (context) => GeneratedGroup10Widget(),
+        '/GeneratedArticlesScrollWidget': (context) =>
+            GeneratedArticlesScrollWidget(),
+        '/GeneratedNewsscrollWidget': (context) => GeneratedNewsscrollWidget(),
+        '/GeneratedNewsdetailWidget': (context) => GeneratedNewsdetailWidget(),
+        '/GeneratedArticledetailWidget1': (context) =>
+            GeneratedArticledetailWidget1(),
+        '/GeneratedArticledetailWidget2': (context) =>
+            GeneratedArticledetailWidget2(),
+        '/GeneratedArticlesWidget1': (context) => GeneratedArticlesWidget1(),
+        '/GeneratedVideosWidget1': (context) => GeneratedVideosWidget1(),
+        '/GeneratedNewsWidget1': (context) => GeneratedNewsWidget1(),
+        // '/GeneratedNewsdetailsWidget': (context) =>
+            // GeneratedNewsdetailsWidget(),
+        '/GeneratedArticledetailsWidget': (context) =>
+            GeneratedArticledetailsWidget(),
+        '/GeneratedHomeScreenarticledetailsWidget': (context) =>
+            GeneratedHomeScreenarticledetailsWidget(),
+        '/GeneratedVideosscrollWidget2': (context) =>
+            GeneratedVideosscrollWidget2(),
+        '/GeneratedBasicdialogWidget': (context) =>
+            GeneratedBasicdialogWidget(),
+        '/GeneratedBasicdialogWidget1': (context) =>
+            GeneratedBasicdialogWidget1(),
+        '/GeneratedNewsscrollWidget1': (context) =>
+            GeneratedNewsscrollWidget1(),
+        '/GeneratedArticlesscrollWidget1': (context) =>
+            GeneratedArticlesscrollWidget1(),
+        '/GeneratedVideosscrollWidget3': (context) =>
+            GeneratedVideosscrollWidget3(),
+        '/GeneratedSearchscrollWidget1': (context) =>
+            GeneratedSearchscrollWidget1(),
+        '/GeneratedRecipedetailsscrollWidget': (context) =>
+            GeneratedRecipedetailsscrollWidget(),
+      },
+    );
+  }
+}
